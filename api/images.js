58d9dc46a1e33585fc5274acfd958f17c07f40aa
Abhi-PSU <2k15.abhi@gmail.com>
2026-04-05 16:41:47 -0400

    broken

--- api/images.js
+++ api/images.js
@@ -1,5 +1,5 @@
 import data from "../lib/api.json" assert { type: "json" };
 
-export default function handler(req, res) {
+module.exports = function handler(req, res)  {
   res.status(200).json(data);
 }
