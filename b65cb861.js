e3b560502fd18a47448dcca31ac041fd282777c6
Abhi-PSU <2k15.abhi@gmail.com>
2026-04-06 00:16:19 +0000

    Deploying to gh-pages from @ Abhi-PSU/instagram-app@4b553a5321aaf841b95c529594b67fd82e33ab1d 🚀

--- b65cb861.js
+++ b65cb861.js
@@ -182,7 +182,7 @@
   margin: 0 auto;
 }
       .card {
-        max-width: 4200px;
+        max-width: 420px;
         margin: 40px auto;
         border: 1px solid #dbdbdb;
         border-radius: 6px;
@@ -212,6 +212,7 @@
   max-height: 500px;
   object-fit: cover;
   display: block;
+  justyfy-cotent: center;
 }
       }
       .card-actions {
